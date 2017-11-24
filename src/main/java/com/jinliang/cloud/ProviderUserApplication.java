7e548c910bbbd9470ba59217ab9c7f46b6029024
金亮 <liang.jin@hand-china.com>
2017-11-24 13:30:13 +0800

    service-user-provicer

--- src/main/java/com/jinliang/cloud/ProviderUserApplication.java
+++ src/main/java/com/jinliang/cloud/ProviderUserApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsSimpleProviderUserApplication {
+public class ProviderUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsSimpleProviderUserApplication.class, args);
+		SpringApplication.run(ProviderUserApplication.class, args);
 	}
 }
